50bddf393d38eb152ac98166d1b252332ec57161
Frugence Fidel <frugencefidel@gmail.com>
2018-02-14 20:01:16 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+app = express(),
+request = require('request'),
+{apikey} = require('./secret'),
+port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  request(`http://www.omdbapi.com/?${apikey}&s=24`, (err, response, body) => {
+    if(!err && response.statusCode === 200) {
+      const movies = JSON.parse(body);
+      const {Search: results} = movies;
+      res.render('index', {results});
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App started at port ${port}`);
+});
