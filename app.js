a50b27fd61de330d608b5437c2c7013fa71e77a5
frugencefidel <frugencefidel@gmail.com>
2018-06-16 17:48:27 +0300

    Style navigation and and mixins

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-app = express(),
-request = require('request'),
-apikey = process.env.APIKEY || require('./secret').apikey,
-apiID = process.env.APIID || require('./secret').apiID,
-port = process.env.PORT || 3000;
+const secret = require('./secret');
+const request = require('request');
+
+const app = express();
+const apikey = process.env.APIKEY || secret.apikey;
+const apiID = process.env.APIID || secret.apiID;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   res.locals.year = new Date().getFullYear();
@@ -14,18 +16,19 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  const search =  req.query.movie || 24;
-  request(`http://www.omdbapi.com/?i=${apikey}&apikey=${apiID}&s=${search}`, (err, response, body) => {
-    if(!err && response.statusCode === 200) {
-      const movies = JSON.parse(body);
-      const {Search: results} = movies;
-      const {Response} = movies;
-      const {Error} = movies;
-      res.render('index', {results, Response, Error});
-    } else if(response.statusCode === 403) {
-      res.redirect('/');
+  const search = req.query.movie || 24;
+  request(
+    `http://www.omdbapi.com/?i=${apikey}&apikey=${apiID}&s=${search}`,
+    (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        const movies = JSON.parse(body);
+        const { Search: results } = movies;
+        const { Response } = movies;
+        const { Error } = movies;
+        res.render('index', { results, Response, Error });
+      }
     }
-  });
+  );
 });
 
 app.listen(port, () => {
